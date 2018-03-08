aa78a08ebc35b0fe1cbd8af535ce6fe05fda7fa1
David Allen <davideallen7@gmail.com>
2018-03-08 09:21:22 -0800

    created migration file, model file for User

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306181842) do
+ActiveRecord::Schema.define(version: 20180308171758) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
