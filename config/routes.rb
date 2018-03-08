aa34182e613ecfe704c1fdeffe609f776f50bbe4
David Allen <davideallen7@gmail.com>
2018-03-08 13:14:06 -0800

    add log in function and form

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   get 'careers', to: 'pages#careers'
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
