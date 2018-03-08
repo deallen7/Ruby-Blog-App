ba05b2efcd813a257929b2514f43585a1466d473
David Allen <davideallen7@gmail.com>
2018-03-08 13:04:18 -0800

    add pagination to views

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ end
 gem 'bootstrap-sass', '~> 3.3.7'
 
 gem 'bcrypt', '~> 3.1.7'
+gem 'will_paginate', '3.1.5'
+gem 'bootstrap-will_paginate', '1.0.0'
 
 gem 'pg', '~> 0.18'
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 	end
 
 	def index
-		@users = User.all
+		@users = User.paginate(page: params[:page], per_page: 2)
 	end
 
 	def create
@@ -34,6 +34,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+		@user_articles = @user.articles.paginate(page: params[:page], per_page: 2)
 	end
 
 	private
