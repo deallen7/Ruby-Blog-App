31af3325b973ac52d2e24b12f011c5e690a3a789
David Allen <davideallen7@gmail.com>
2018-03-08 14:33:52 -0800

    restrict actions in ui

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:edit, :update, :show]
 
 	def new
 		@user = User.new
@@ -19,11 +20,9 @@ class UsersController < ApplicationController
 	end
 
 	def edit 
-		@user = User.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
 		if @user.update(user)
 			flash[:success] = "Your account was updated successfully"
 			redirect_to articles_path
@@ -33,8 +32,11 @@ class UsersController < ApplicationController
 	end 
 
 	def show
-		@user = User.find(params[:id])
 		@user_articles = @user.articles.paginate(page: params[:page], per_page: 2)
+	end
+
+	def set_user
+		@user = User.find(params[:id])
 	end
 
 	private
