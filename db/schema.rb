f06734c9e64a6fb5d044a8dab6f191784ce6dd03
David Allen <davideallen7@gmail.com>
2018-03-08 09:54:04 -0800

    add association btw user and articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308171758) do
+ActiveRecord::Schema.define(version: 20180308174000) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
