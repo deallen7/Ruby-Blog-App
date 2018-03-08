74beef6fc3da75700660f4a3135e3a2820a13a6f
David Allen <davideallen7@gmail.com>
2018-03-08 15:29:18 -0800

    complete authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	before_action :set_user, only: [:edit, :update, :show]
+	before_action :require_same_user, only: [:edit, :update]
 
 	def new
 		@user = User.new
@@ -43,5 +44,12 @@ class UsersController < ApplicationController
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
 	end
+
+	def require_same_user
+		if current_user != @user
+			flash[:danger] = "You can only edit your own account"
+			redirect_to root_path
+		end
+	end 
 
 end
