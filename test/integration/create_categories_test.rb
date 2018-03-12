af226d4ab088cdcc1042f2936ba4d8b45dd38002
David Allen <davideallen7@gmail.com>
2018-03-12 11:28:17 -0700

    add paginate for category view

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -15,4 +15,24 @@ assert_template 'categories/index'
 assert_match "sports", response.body
 end
 
+test "invalid category submission results in failure" do
+	get new_category_path
+	assert_template 'categories/new'
+	assert_no_difference 'Category.count' do
+	#post_via_redirect categories_path, category: {name: "sports"}
+	# if using Rails 5 use below 2 lines instead of line above (without the comment tag of course):
+	 post categories_path, category: { name: " " }
+	end
+	assert_template 'categories/new'
+	assert_select 'h2.panel-title'
+	assert_select 'div.panel-body'
 end
+
+end
+
+
+
+
+
+
+
